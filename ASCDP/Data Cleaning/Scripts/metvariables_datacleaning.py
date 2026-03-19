6dc176aeee0967832470cc481803901be7ec00b0
lmclatch <lmclatch@hawaii.edu>
2026-03-18 19:11:04 -0600

    new rain in tot aasu run

--- ASCDP/Data Cleaning/Scripts/metvariables_datacleaning.py
+++ ASCDP/Data Cleaning/Scripts/metvariables_datacleaning.py
@@ -247,9 +247,9 @@ print(f"Final columns:\n{combined_df.columns.tolist()}")
 # combined_df = combined_df.drop(columns=['SlrMJ_Tot_Poloa'],  errors='ignore')
 # combined_df = combined_df.drop(columns=['SlrW_Avg_Poloa'],   errors='ignore')
 # combined_df = combined_df.drop(columns=['SlrW_Avg_Vaipito'], errors='ignore')
-#combined_df = combined_df.drop(columns=['RH_Aasu'],          errors='ignore')
+combined_df = combined_df.drop(columns=['RH_Aasu'],          errors='ignore')
 combined_df = combined_df.drop(columns=['AirTF_Avg_Aasu'],   errors='ignore')
-combined_df = combined_df.drop(columns=['Rain_in_Tot_Aasu'], errors='ignore')
+#combined_df = combined_df.drop(columns=['Rain_in_Tot_Aasu'], errors='ignore')
 
 # ==============================================================================
 # Date config
@@ -330,9 +330,9 @@ def create_train_pred_splits(df, target_station, target_variable, config_dict,
 df_train, df_pred = create_train_pred_splits(
     combined_df,
     target_station='Aasu',
-    target_variable='RH',
+    target_variable='Rain_in_Tot',
     config_dict=date_config
 )
 
-df_train.to_csv("/Users/lizamclatchy/Documents/GitHub/ASPA_HistoricalDataCleaning/ASCDP/Data Cleaning/Cleaned Model Input Data/aasu_rh_train.csv", index=False)
+df_train.to_csv("/Users/lizamclatchy/Documents/GitHub/ASPA_HistoricalDataCleaning/ASCDP/Data Cleaning/Cleaned Model Input Data/aasu_Rain_in_Tot_train.csv", index=False)
 # df_pred.to_csv(".../vaipito_SlrMJ_Tot_pred.csv", index=False)
